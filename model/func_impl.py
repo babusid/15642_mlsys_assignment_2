23ba621c244e55d2059843123e864dfabf4ca227
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2026-02-28 14:43:13 -0500

    passing part 6

--- model/func_impl.py
+++ model/func_impl.py
@@ -321,8 +321,7 @@ def megatron_collect_backward_output(
     """TODO: Your code here"""
 
     # Hint: your implementation should be within one line of code
-
-    raise NotImplementedError
+    return output_grad
 
 
 def megatron_collect_backward_x(
@@ -353,8 +352,7 @@ def megatron_collect_backward_x(
     """TODO: Your code here"""
 
     # Hint: your implementation should be within one line of code
-
-    raise NotImplementedError
+    return grad_x
 
 
 def collect_weight_grad(
